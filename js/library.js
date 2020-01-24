3894c8f22cef99c7d6893bf9c8a8cccb2aca3e43
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-01-24 19:05:09 -0300

    Created addBook function to get elements by id, created the variable to create a new book

--- js/library.js
+++ js/library.js
@@ -1,13 +1,22 @@
+
 let myLibrary = [];
 
 function Book(title,author,numPages) {
     this.title = title
     this.author = author
     this.numPages = numPages
-  // the constructor...
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book)
-    // do stuff here
+function addBookToLibrary() {
+  let title= document.getElementById ("booktitle").value; 
+  let author= document.getElementById ("author").value;
+  let numPages= document.getElementById ("pages").value;
+
+  let newBook = new Book (title,author,numPages)
+  myLibrary.push(newBook);
+  //console.log(myLibrary);
+}
+
+function render() {
+  document.write(myLibrary);
 }
