60e33ac6372b1beb45dd3fa5c80823a83d9763cc
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-01-27 19:31:35 -0300

    input type on index for button deleted, starting the process of the read or not read in JS

--- js/library.js
+++ js/library.js
@@ -12,20 +12,39 @@ let myLibrary = [
 ];
 
 function Book(title, author, numPages) {
-  this.title = title;
-  this.author = author;
-  this.numPages = numPages;
+ this.title = document.getElementById("booktitle").value;
+  this.author = document.getElementById("author").value;
+  this.numPages = document.getElementById("pages").value;
+  this.read = false;
 }
 
 function addBookToLibrary() {
-  let title = document.getElementById("booktitle").value;
-  let author = document.getElementById("author").value;
-  let numPages = document.getElementById("pages").value;
+
 
   let newBook = new Book(title, author, numPages);
   myLibrary.push(newBook);
   //console.log(myLibrary);
 }
+
+/*Book.prototype = {
+  constructor: Book,
+  updateRead () {
+    if (this.readAlready === 'been reading'){
+      this.readAlready = 'not been reading';
+    } else {
+      this.readAlready = 'been reading';
+    }
+  },
+};
+
+  /*let updateRead = document.createElement("Already read?");
+  if updateRead () {
+    cell4.innerHTML = "already read that book";
+  }
+  else {
+  cell4.innerHTML = "book not read yet";
+  }
+}*/
 
 function render() {
   const books = document.getElementById("books");
@@ -36,12 +55,20 @@ function render() {
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
 
     cell1.innerHTML = myLibrary[i].title;
     cell2.innerHTML = myLibrary[i].author;
-    cell3.innerHTML = myLibrary[i].numPages;
+    cell3.innerHTML = myLibrary[i].numPages; 
+    //let updateRead = document.getElementById("myBtn");
+    cell4.innerHTML = myLibrary[i].read;
   }
 }
 
 render();
 
+/*if (myLibrary.read) {
+cell4.innerHTML = "already read that book"
+}
+else{
+cell4.innerHTML = "book not read yet"}*/
