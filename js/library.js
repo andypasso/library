a393e1ac6ea52d4efc885602f31f868a12f7d71e
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-01-29 18:52:18 -0300

    readme updated, style on the table modified, linters errors fixed

--- js/library.js
+++ js/library.js
@@ -1,4 +1,4 @@
-let myLibrary = JSON.parse(window.localStorage.getItem("libraryStorage"));
+let myLibrary = JSON.parse(window.localStorage.getItem('libraryStorage'));
 if (!myLibrary) myLibrary = [];
 
 function Book(title, author, numPages) {
@@ -9,12 +9,12 @@ function Book(title, author, numPages) {
 }
 
 function addBookToLibrary() {
-  let title = document.getElementById("booktitle").value;
-  let author = document.getElementById("author").value;
-  let numPages = document.getElementById("pages").value;
-  let newBook = new Book(title, author, numPages);
+  const title = document.getElementById('booktitle').value;
+  const author = document.getElementById('author').value;
+  const numPages = document.getElementById('pages').value;
+  const newBook = new Book(title, author, numPages);
 
-  if (title === "" || author === "" || numPages <= 0 || numPages === "") {
+  if (title === '' || author === '' || numPages <= 0 || numPages === '') {
     showError();
     return;
   }
@@ -25,12 +25,12 @@ function addBookToLibrary() {
 }
 
 function renderBook(books, i, myLibrary) {
-  let row = books.insertRow(0);
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
-  let cell4 = row.insertCell(3);
-  let cell5 = row.insertCell(4);
+  const row = books.insertRow(0);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  const cell5 = row.insertCell(4);
   cell1.innerHTML = myLibrary[i].title;
   cell2.innerHTML = myLibrary[i].author;
   cell3.innerHTML = myLibrary[i].numPages;
@@ -39,61 +39,57 @@ function renderBook(books, i, myLibrary) {
 }
 
 function render() {
-  const books = document.getElementById("books");
-  books.innerHTML = "";
-  for (let i = myLibrary.length - 1; i >= 0; i--) {
+  const books = document.getElementById('books');
+  books.innerHTML = '';
+  for (let i = myLibrary.length - 1; i >= 0; i -= 1) {
     renderBook(books, i, myLibrary);
   }
-  let buttons = document.querySelectorAll(".readButton");
-  buttons.forEach(buttonElement =>
-    buttonElement.addEventListener("click", updateReadStatus)
-  );
-  let deletebuttons = document.querySelectorAll(".deleteButton");
-  deletebuttons.forEach(deleteElement =>
-    deleteElement.addEventListener("click", deleteBook)
-  );
+  const buttons = document.querySelectorAll('.readButton');
+  buttons.forEach(buttonElement => buttonElement.addEventListener('click', updateReadStatus));
+  const deletebuttons = document.querySelectorAll('.deleteButton');
+  deletebuttons.forEach(deleteElement => deleteElement.addEventListener('click', deleteBook));
 }
 
 render();
 
-let addNewBook = document.getElementById("newBookButton");
-addNewBook.addEventListener("click", addBookToLibrary);
+const addNewBook = document.getElementById('newBookButton');
+addNewBook.addEventListener('click', addBookToLibrary);
 
 function updateReadStatus(event) {
-  let button = event.target;
-  let index = button.getAttribute("data-index");
+  const button = event.target;
+  const index = button.getAttribute('data-index');
 
   myLibrary[index].read = !myLibrary[index].read;
   if (myLibrary[index].read) {
-    button.innerHTML = "Already Read";
+    button.innerHTML = 'Already Read';
   } else {
-    button.innerHTML = "Unread";
+    button.innerHTML = 'Unread';
   }
   updateLocalStorage(myLibrary);
 }
 
 function deleteBook(deletebook) {
-  let button = deletebook.target;
-  let index = button.getAttribute("data-index");
-  alert("Are you sure you want to delete this book?");
+  const button = deletebook.target;
+  const index = button.getAttribute('data-index');
+  alert('Are you sure you want to delete this book?');
   myLibrary.splice(index, 1);
   updateLocalStorage(myLibrary);
   render();
 }
 
-function validateForm() {
-  let x = document.forms["formSection"]["title"].value;
-  if (x == "") {
-    alert("Name must be filled out");
+function validateForm() { // estaba vacio ()
+  const x = document.forms.formSection.title.value;
+  if (x === '') {
+    alert('Name must be filled out');
     return false;
   }
-  let y = document.forms["formSection"]["author"].value;
-  if (y == "") {
-    alert("author must be filled out");
+  const y = document.forms.formSection.author.value;
+  if (y === '') {
+    alert('author must be filled out');
     return false;
   }
 }
 
 function updateLocalStorage(array) {
-  window.localStorage.setItem("libraryStorage", JSON.stringify(array));
+  window.localStorage.setItem('libraryStorage', JSON.stringify(array));
 }
