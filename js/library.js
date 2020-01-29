9cf7ee0c8990bc932880ad4445cc8d20e75dac5a
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-01-28 21:53:30 -0300

    added readme, stickler and linter for js, added styles to the app, added localstorage, created the read and unread function, created the delete function

--- js/library.js
+++ js/library.js
@@ -1,50 +1,23 @@
-let myLibrary = [
-  {
-    title: "book 1",
-    author: "author 1",
-    numPages: "100"
-  },
-  {
-    title: "book 2",
-    author: "author 2",
-    numPages: "200"
-  }
-];
+let myLibrary = JSON.parse(window.localStorage.getItem('libraryStorage'));
+if (!myLibrary) myLibrary = [];
 
 function Book(title, author, numPages) {
- this.title = document.getElementById("booktitle").value;
-  this.author = document.getElementById("author").value;
-  this.numPages = document.getElementById("pages").value;
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
   this.read = false;
 }
 
 function addBookToLibrary() {
-
-
+  let title = document.getElementById("booktitle").value;
+  let author = document.getElementById("author").value;
+  let numPages = document.getElementById("pages").value;
   let newBook = new Book(title, author, numPages);
   myLibrary.push(newBook);
-  //console.log(myLibrary);
+  updateLocalStorage(myLibrary)
+  render();
 }
 
-/*Book.prototype = {
-  constructor: Book,
-  updateRead () {
-    if (this.readAlready === 'been reading'){
-      this.readAlready = 'not been reading';
-    } else {
-      this.readAlready = 'been reading';
-    }
-  },
-};
-
-  /*let updateRead = document.createElement("Already read?");
-  if updateRead () {
-    cell4.innerHTML = "already read that book";
-  }
-  else {
-  cell4.innerHTML = "book not read yet";
-  }
-}*/
 
 function render() {
   const books = document.getElementById("books");
@@ -56,19 +29,48 @@ function render() {
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
 
     cell1.innerHTML = myLibrary[i].title;
     cell2.innerHTML = myLibrary[i].author;
     cell3.innerHTML = myLibrary[i].numPages; 
-    //let updateRead = document.getElementById("myBtn");
-    cell4.innerHTML = myLibrary[i].read;
+    cell4.innerHTML = `<button class='readButton' data-index= ${i}>Unread</button>`
+    cell5.innerHTML = `<button class='deleteButton' data-index= ${i}>Delete</button>`
   }
-}
 
-render();
+  let buttons= document.querySelectorAll(".readButton")
+  buttons.forEach(buttonElement => buttonElement.addEventListener("click", updateReadStatus));
+  let deletebuttons= document.querySelectorAll(".deleteButton")
+  deletebuttons.forEach(deleteElement => deleteElement.addEventListener("click", deleteBook));
 
-/*if (myLibrary.read) {
-cell4.innerHTML = "already read that book"
 }
-else{
-cell4.innerHTML = "book not read yet"}*/
+  render();
+
+  let addNewBook= document.getElementById("newBookButton") 
+  addNewBook.addEventListener("click", addBookToLibrary);
+
+  function updateReadStatus(event) {
+    let button = event.target 
+    let index = button.getAttribute("data-index");
+
+    myLibrary[index].read= !myLibrary[index].read
+    if (myLibrary[index].read) {
+      button.innerHTML= "Already Read";
+    } else {
+      button.innerHTML= "Unread";
+    }
+    updateLocalStorage(myLibrary);
+  }
+
+  function deleteBook(deletebook) {
+    let button = deletebook.target 
+    let index = button.getAttribute("data-index");
+    alert("Are you sure you want to delete this book?");
+    myLibrary.splice(index, 1);
+    updateLocalStorage(myLibrary);
+    render();
+  }
+
+function updateLocalStorage(array) {
+  window.localStorage.setItem('libraryStorage', JSON.stringify( array ));
+} 
